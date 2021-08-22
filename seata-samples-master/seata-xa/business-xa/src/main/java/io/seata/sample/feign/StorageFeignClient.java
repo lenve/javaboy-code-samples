a52387d477c8b9eb47c5c33b2f9d32b9370338df
wangsong <wangsong0210@gmail.com>
2021-08-22 11:07:45 +0800

    m

--- seata-samples-master/seata-xa/business-xa/src/main/java/io/seata/sample/feign/StorageFeignClient.java
+++ seata-samples-master/seata-xa/business-xa/src/main/java/io/seata/sample/feign/StorageFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "storage-xa", url = "127.0.0.1:8081")
+@FeignClient(name = "storage-xa", url = "127.0.0.1:8085")
 public interface StorageFeignClient {
 
     @GetMapping("/deduct")
