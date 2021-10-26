1c98eb379ae2bff8fae171a702d128735b42f7fc
wangsong <wangsong0210@gmail.com>
2021-10-26 22:27:08 +0800

    m

--- batch_insert/src/test/java/org/javaboy/batch_insert/BatchInsertApplicationTests.java
+++ batch_insert/src/test/java/org/javaboy/batch_insert/BatchInsertApplicationTests.java
@@ -5,8 +5,13 @@ import org.javaboy.batch_insert.model.User;
 import org.javaboy.batch_insert.service.IUserService;
 import org.javaboy.batch_insert.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,13 +19,17 @@ import java.util.List;
 @SpringBootTest
 class BatchInsertApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(BatchInsertApplicationTests.class);
     @Autowired
     UserService userService;
 
     /**
+     *
+     * 单元测试加事务的目的是为了插入之后自动回滚，避免影响下一次测试结果
      * 一条一条插入
      */
     @Test
+    @Transactional
     void addUserOneByOne() {
         List<User> users = new ArrayList<>();
         for (int i = 0; i < 50000; i++) {
@@ -38,6 +47,7 @@ class BatchInsertApplicationTests {
      * mp 批量插入
      */
     @Test
+    @Transactional
     void mpBatchInsert() {
         List<User> users = new ArrayList<>();
         for (int i = 0; i < 50000; i++) {
@@ -50,7 +60,7 @@ class BatchInsertApplicationTests {
         long startTime = System.currentTimeMillis();
         userService.saveBatch(users);
         long endTime = System.currentTimeMillis();
-
+        logger.info("MyBatis Plus 批量插入耗时 {}", (endTime - startTime));
     }
 
 
@@ -58,6 +68,7 @@ class BatchInsertApplicationTests {
      * 合并成一条 SQL 插入
      */
     @Test
+    @Transactional
     void addByOneSQL() {
         List<User> users = new ArrayList<>();
         for (int i = 0; i < 50000; i++) {
