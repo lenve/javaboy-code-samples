e65d54bedc9ed2f18d26f7c17e6ef19fed1a893d
wangsong <wangsong0210@gmail.com>
2022-02-11 20:48:32 +0800

    m

--- jpa_one_to_many/src/test/java/org/javaboy/jpa_one_to_many/JpaOneToManyApplicationTests.java
+++ jpa_one_to_many/src/test/java/org/javaboy/jpa_one_to_many/JpaOneToManyApplicationTests.java
@@ -1,13 +1,36 @@
 package org.javaboy.jpa_one_to_many;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class JpaOneToManyApplicationTests {
 
+    @Autowired
+    SchoolRepository schoolRepository;
+
+    @Test
+    void test01() {
+        List<School> list = schoolRepository.findSchoolByAddressProvince("黑龙江");
+        System.out.println("list = " + list);
+    }
+
     @Test
     void contextLoads() {
+        School school = new School();
+        school.setSid(1);
+        school.setName("哈佛大学");
+        Address address = new Address();
+        address.setAid(1);
+        address.setProvince("黑龙江");
+        address.setCity("哈尔滨");
+        address.setArea("某地");
+        address.setPhone("123456");
+        school.setAddress(address);
+        schoolRepository.save(school);
     }
 
 }
